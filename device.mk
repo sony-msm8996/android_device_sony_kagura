c60db07e8f36ae7e0d4ab0fa99c047e03389695d
Olivier Karasangabo <olivier@lineageos.org>
2020-06-19 14:57:15 +0200

    keyaki: Remove TFA calibration script
    Change-Id: I4b8197a51a8607110c959fe701282520c0052cec

--- device.mk
+++ device.mk
@@ -64,11 +64,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
-# TFA calibration
-PRODUCT_PACKAGES += \
-    init.tfa.sh \
-    tinyplay
-
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
