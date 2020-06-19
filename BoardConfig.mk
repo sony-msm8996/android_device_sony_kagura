bbc514219d37f09ebf8bfa544fca9f1ca3986d50
Olivier Karasangabo <olivier@lineageos.org>
2020-06-19 14:18:13 +0200

    keyaki: Drop assertions on modem version
    Change-Id: I692004fa2d9d91908bbf149011eaea9443781ec1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,9 +23,6 @@ DEVICE_PATH := device/sony/keyaki
 # Assert
 TARGET_OTA_ASSERT_DEVICE := keyaki,keyaki_dsds,G8231,G8232
 
-# Board
-TARGET_BOARD_INFO_FILE ?= $(DEVICE_PATH)/board-info.txt
-
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
